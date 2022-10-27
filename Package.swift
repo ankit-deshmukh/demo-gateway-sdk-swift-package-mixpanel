c03ab26418e9c7f1b11582122367199ab7dd2afa
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-10-27 20:21:59 +0530

    build: added a wrapper target for dependencies

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "SCGateway",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SCGateway",
-            targets: ["SCGateway"]),
+            targets: ["SCGatewayWrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +21,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SCGatewayWrapper",
+            dependencies: [
+                Target.Dependency
+                .target(name: "SCGateway", condition: .when(platforms: .some([.iOS])))
+            ]
+        ),
         .binaryTarget(name: "SCGateway",
                       url: "https://gateway.smallcase.com/scgateway_mixpanel/1.0.5/SCGateway.xcframework.zip",
                       checksum: "f3be31146874c9c9b5518102dc3fd38b7a126aa84bb3b3dd5b96c3dfd50da9aa"),
